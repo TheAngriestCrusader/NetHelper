556a309d83d6b01dee32fa3c65ce7ab5e55af291
TheAngriestCrusader <theangriestcrusader@gmail.com>
2025-04-24 12:01:11 +0100

    Add missing information in createClient function of ./src/client/client.ts

--- src/client/client.ts
+++ src/client/client.ts
@@ -2,7 +2,8 @@ export default function createClient(host: string, port: number) {
     /**
      * Create a Fastify client instance on the specified host and port.
      * 
-     * @param {string}
+     * @param {string} host - Host address of the server.
+     * @param {number} port - Port that the server listens on.
      */
    console.log('Placeholder.')
 }
