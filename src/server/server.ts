1e50d98f5e656d1e56bddcb15a8633ef5cd28643
TheAngriestCrusader <theangriestcrusader@gmail.com>
2025-04-24 09:50:22 +0100

    Create basic createServer function in server.ts and allow clients to get /time

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,1 +1,25 @@
-console.log("Hello World!")
+import Fastify, { FastifyInstance } from 'fastify';
+
+export default function createServer(host: string, port: number) {
+    const fastify = Fastify({
+        logger: true
+    });
+
+    fastify.get('/time', async (request, reply) => {
+        console.log(`Received request for time from ${request.ip}`);
+        reply.send({ time: new Date().toISOString() });
+    });
+
+    fastify.listen({
+        host: host,
+        port: port
+    }, (err, address) => {
+
+        if (err) {
+            fastify.log.error(err);
+            process.exit(1);
+        }
+
+        fastify.log.info(`Server listening at ${address}`);
+    });
+}
