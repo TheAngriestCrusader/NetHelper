d29b750d3e2baab5f43408b8b967c24d5367703d
TheAngriestCrusader <theangriestcrusader@gmail.com>
2025-04-24 12:29:54 +0100

    Add ./config directory and create serverDetails.json

--- src/core/app.ts
+++ src/core/app.ts
@@ -1,5 +1,9 @@
 import dotenv from 'dotenv';
+import fs from 'fs';
 import createClient from '../client/client';
 import createServer from '../server/server';
 
 dotenv.config();
+const serverDetails = JSON.parse(fs.readFileSync('./config/serverDetails.json', 'utf-8'));
+
+console.log(serverDetails.host);
